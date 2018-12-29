09b55b7cb1bc7a330a8c686cda36ccf9b488c409
Elisha Yadgaran <ElishaY@alum.mit.edu>
2018-12-29 01:17:11 -0500

    removed extraneous constants

--- backend/modeling/constants.py
+++ backend/modeling/constants.py
@@ -1,14 +1,6 @@
 # Global constants
 # special tokens
 PAD_TOKEN = "#PAD#"
-UNK_TOKEN = "#UNK#"
 START_TOKEN = "#START#"
 END_TOKEN = "#END#"
 PAD_LENGTH = 20
-
-SPECIAL_TOKEN_MAP = {  # Use negative index to avoid clashing with vocab
-    PAD_TOKEN: -1,
-    UNK_TOKEN: -2,
-    START_TOKEN: -3,
-    END_TOKEN: -4,
-}
