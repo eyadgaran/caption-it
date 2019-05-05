c0ca84e4d7dc8be660d0899ca66a6acd5f70591f
Elisha Yadgaran <ElishaY@alum.mit.edu>
2019-05-05 19:59:04 -0400

    updated db connection

--- backend/database/initialization.py
+++ backend/database/initialization.py
@@ -5,49 +5,18 @@ Module for database initialization
 __author__ = 'Elisha Yadgaran'
 
 
-from simpleml.utils.initialization import Database
-from backend.database.models import BaseModel
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-import os
-from os.path import join, dirname, abspath
-import configparser
+from simpleml.utils import Database
+from .models import BaseModel
 
 
-def parse_cnf(cnf_section):
-    '''
-    Assumes there is a cnf file located at ~/.my.cnf with sections and parameters
-    :param cnf_section: cnf section title for param group
-    :return: dictionary of parameters (None defaults to empty string)
-    '''
-    config = configparser.SafeConfigParser(allow_no_value=True)
-    config.read(os.getenv('CNF_FILE',
-                          join(dirname(abspath(__file__)), 'secret.cnf')))
-    parameter_dict = dict(config.items(cnf_section))
-
-    return parameter_dict
-
-
-class SimpleMLDatabase(object):
+class SimpleMLDatabase(Database):
     def __init__(self):
-        self.database_params = parse_cnf('captioner-simpleml')
+        super(SimpleMLDatabase, self).__init__(configuration_section='simpleml-captioner')
+
+
+class AppDatabase(Database):
+    def __init__(self):
+        super(AppDatabase, self).__init__(configuration_section='app-captioner')
 
     def initialize(self, **kwargs):
-        db = Database(**self.database_params)
-        db.initialize(**kwargs)
-
-
-class AppDatabase(object):
-    def __init__(self):
-        self.database_params = parse_cnf('captioner-app')
-
-    def initialize(self):
-        url = '{jdbc}://{user}:{password}@{host}:{port}/{database}'.format(
-            **self.database_params)
-        engine = create_engine(url)
-        db_session = scoped_session(sessionmaker(autocommit=True,
-                                                 autoflush=False,
-                                                 bind=engine))
-        # Create tables.
-        BaseModel.metadata.create_all(bind=engine)
-        BaseModel.set_session(db_session)
+        self._initialize(BaseModel, **kwargs)
--- backend/database/models.py
+++ backend/database/models.py
@@ -1,3 +1,10 @@
+'''
+Module for database tables
+'''
+
+__author__ = 'Elisha Yadgaran'
+
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Numeric, DateTime, func, BigInteger
 from sqlalchemy_mixins import AllFeaturesMixin
@@ -11,3 +18,10 @@ class BaseModel(Base, AllFeaturesMixin):
     created_timestamp = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
     modified_timestamp = Column(DateTime(timezone=True), nullable=True, server_onupdate=func.now())
     id = Column(BigInteger, primary_key=True)
+
+
+class Feedback(BaseModel):
+    __tablename__ = 'feedback'
+
+    feedback = Column(String(1200), nullable=False)
+
